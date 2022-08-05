52325023ebd0dd89023af4c4f0588e3f57d5da67
Mikhail Konkov <kmapc4@gmail.com>
2022-08-05 12:11:58 +0300

    add Example005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "мак")
+{
+    Console.WriteLine("Аве, цезарь МАК!!! Какая встреча!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
